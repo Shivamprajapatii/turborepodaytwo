36d090f3ec2eb71fca801d041ebabd53ae5c870b
shivamprajapatii <shivamprajapati4794@gmail.com>
2025-03-21 19:19:35 +0530

    start frehs today

--- packages/ui/src/Input.tsx
+++ packages/ui/src/Input.tsx
@@ -1,10 +1,13 @@
 type PropType = {
     placeholder : string
     onChange : () => void
+    size : "big" | "small"
 };
 
-export default function InputBox({placeholder} : PropType) {
+export default function InputBox({placeholder, size } : PropType) {
     return(
-        <input type="text" placeholder={placeholder}></input>
+        <input type="text" placeholder={placeholder} style={{
+            fontWeight : size === "big" ? 20 : 10
+        }}></input>
     )
 }
