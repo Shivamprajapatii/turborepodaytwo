af539727d1e22f6c0ca7a77ec05129e5cfe5ce34
shivamprajapatii <shivamprajapati4794@gmail.com>
2025-03-21 19:31:57 +0530

    chat/anything request come handle by the page.tsx

---
+++ apps/web/app/chat/[randomId]/page.tsx
@@ -0,0 +1,7 @@
+export default function ChatRoom() {
+    return(
+        <div>
+            <h1>Hiiii th</h1>
+        </div>
+    )
+}
--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,10 +1,18 @@
+"use client"
+
 import  Input  from "@repo/ui/Input"
+import { useRouter } from "next/navigation"
 
 export default function Home() {
+  const path = 'dsafjafd2093123';
+  const router = useRouter();
   return (
     <div>
       <h1>Hii thire</h1>
-      <Input onChange={() => { alert("change")}} placeholder="Eneter" size="big"/>
+      <Input onChange={() => {}} placeholder="Eneter" size="big"/>
+      <button onClick={() => {
+        router.push(`chat/${path}`)
+      }}>Join Room</button>
     </div>
   ) 
 }
